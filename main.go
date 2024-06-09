fb5f22a80c1c34167cef61ea642a3a2fa2c76176
哀川悯 <1368447941@qq.com>
2024-06-09 17:36:08 +0800

    web demo

---
+++ main.go
@@ -0,0 +1,138 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	_ "github.com/go-sql-driver/mysql"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+type Comment struct {
+	ID      int
+	Name    string `json:"name"`
+	Content string `json:"content"`
+}
+
+var db *gorm.DB
+var err error
+
+// 连接数据库
+func initSQL() {
+	//配置MySQL连接参数
+	username := "root"        //账号
+	password := "123456"      //密码
+	host := "127.0.0.1"       //数据库地址，可以是Ip或者域名
+	port := 3306              //数据库端口
+	Dbname := "xlab_comments" //数据库名
+	timeout := "10s"          //连接超时，10秒
+
+	dsn := fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8&parseTime=True&loc=Local&timeout=%s", username, password, host, port, Dbname, timeout)
+
+	db, err = gorm.Open(mysql.Open(dsn), &gorm.Config{})
+	if err != nil {
+		panic("连接数据库失败, error=" + err.Error())
+	} else {
+		fmt.Println("Connected")
+	}
+}
+
+func main() {
+	initSQL()
+	fmt.Println("Server Start")
+	http.HandleFunc("/comment/get", getComment)
+	http.HandleFunc("/comment/add", addComment)
+	http.HandleFunc("/comment/delete", deleteComment)
+
+	fs := http.FileServer(http.Dir('.'))
+	http.Handle("/", fs)
+
+	_ = http.ListenAndServe(":8080", nil)
+
+}
+
+// 获取评论
+func getComment(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	currentPage, _ := strconv.Atoi(r.Form.Get("page"))
+	size, _ := strconv.Atoi(r.Form.Get("size"))
+
+	fmt.Println("GO:getComment ", currentPage, size)
+	// 处理GET请求，返回评论列表
+	// 查询数据库获取评论列表
+	var comments []Comment
+
+	db.Order("ID desc").Limit(size).Offset((currentPage - 1) * size).Find(&comments)
+
+	// 返回评论列表
+	var count int64
+	var tmp_comments []Comment
+	db.Find(&tmp_comments).Count(&count)
+
+	fmt.Println("Got:", comments, "|total:", count)
+	data := map[string]interface{}{
+		"total":    count,
+		"comments": comments,
+	}
+
+	response := map[string]interface{}{
+		"code": 0,
+		"msg":  "success",
+		"data": data,
+	}
+	response_json, _ := json.Marshal(response)
+
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response_json)
+}
+
+// 添加评论
+func addComment(w http.ResponseWriter, r *http.Request) {
+	var comment Comment
+	_ = json.NewDecoder(r.Body).Decode(&comment)
+	var topComment Comment
+	db.Last(&topComment)
+	// 新评论的id是最大id+1
+	comment.ID = topComment.ID + 1
+	fmt.Println(comment)
+	db.Create(&comment)
+
+	// 返回添加成功的提示信息
+
+	data := map[string]interface{}{
+		"id":      comment.ID,
+		"name":    comment.Name,
+		"content": comment.Content,
+	}
+
+	response := map[string]interface{}{
+		"code": 0,
+		"msg":  "success",
+		"data": data,
+	}
+	response_json, _ := json.Marshal(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response_json)
+}
+
+// 按照id删除评论
+func deleteComment(w http.ResponseWriter, r *http.Request) {
+	r.ParseForm()
+	var comment Comment
+	db.First(&comment, r.Form.Get("id"))
+	fmt.Println("deleting:", comment, "|id:", r.Form.Get("id"))
+	db.Delete(&comment)
+
+	// 返回删除成功的提示信息
+	response := map[string]interface{}{
+		"code": 0,
+		"msg":  "success",
+		"data": nil,
+	}
+	response_json, _ := json.Marshal(response)
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(response_json)
+}
---
+++ main.js
@@ -0,0 +1,158 @@
+var currentPage = 1;
+const pageSize = 5;
+var totalPage
+
+const url = "127.0.0.1";
+
+var commit = document.getElementById("commit");
+var username = document.getElementById("userName");
+var commentcontent = document.getElementById("commentContent");
+
+
+window.onload = function ()
+{
+    render();
+    commit = document.getElementById("commit");
+    username = document.getElementById("userName");
+    commentcontent = document.getElementById("commentContent");
+}
+
+function render()
+{
+    var myRequest = new Request(`/comment/get?page=${currentPage}&size=${pageSize}`, {
+        method: "GET",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+
+    fetch(myRequest)
+        .then(response => response.json())
+        .then(res =>
+        {
+            console.log(res)
+            show_comments(res.data.comments);
+
+            console.log("rendering page number!")
+            totalPage = Math.ceil(res.data.total / pageSize);
+            if (totalPage == 0) totalPage = 1;
+            console.log(res.data.total, pageSize, totalPage);
+            var page = document.getElementById("pagex");
+            while (page.firstChild)
+            {
+                page.removeChild(page.firstChild);
+            }
+            var pagetext = document.createElement("span");
+            pagetext.innerHTML = `<strong>${currentPage}/${totalPage}</strong>`;
+            page.appendChild(pagetext);
+        })
+}
+
+// 添加评论
+commit.addEventListener("click", () =>
+{
+    if (username.value == "" || commentcontent.value == "")
+    {
+        alert("请输入用户名和评论内容😡");
+        return;
+    }
+    var myRequest = new Request(`/comment/add`, {
+        method: "POST",
+        body: JSON.stringify({
+            "name": username.value,
+            "content": commentcontent.value
+        }),
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    fetch(myRequest)
+        .then(response => response.json())
+        .then(data =>
+        {
+            console.log(data);
+        })
+        .then(render)
+    clear_input();
+})
+
+
+
+function show_comments(comments)
+{
+    var div = document.getElementById("comments");
+    while (div.firstChild)
+    {
+        div.removeChild(div.firstChild);
+    }
+
+    for (var i = 0; i < comments.length; i++)
+    {
+        var comment = comments[i];
+        var commentElement = document.createElement("div");
+        commentElement.className = "commentsZone";
+        commentElement.innerHTML = `
+        <div class="userName_commentContent">
+            <h3>${comment.name}</h3>
+        </div>
+
+        <div class="userName_commentContent">
+            <h4>${comment.content}</h4>
+        </div>
+
+        <div class="buttonLine">
+            <button class="buttonStyle" id="delete${comment.ID}">删除</button>
+        </div>
+        `;
+        div.appendChild(commentElement);
+    }
+
+    var buttons = document.querySelectorAll('[id^="delete"]');
+    buttons.forEach(button =>
+    {
+        button.addEventListener("click", () =>
+        {
+            var id = button.id.replace("delete", "");
+            delete_comment(id);
+        })
+    })
+}
+
+function delete_comment(id)
+{
+    var myRequest = new Request(`/comment/delete?id=${id}`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        }
+    })
+    fetch(myRequest).then(() => render())
+}
+
+function clear_input()
+{
+    username.value = "";
+    commentContent.value = "";
+}
+
+
+var PREV = document.getElementById("previous");
+var NEXT = document.getElementById("next");
+
+PREV.addEventListener("click", () =>
+{
+    if (currentPage >= 2)
+    {
+        currentPage--;
+        render();
+    }
+})
+
+NEXT.addEventListener("click", () =>
+{
+    if (currentPage < totalPage)
+    {
+        currentPage++;
+        render();
+    }
+})
